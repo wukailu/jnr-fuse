43fea7b8563a4d76530fd74035d5a80eec59f6af
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-12-13 20:57:55 +0800

    test 4 multi thread

---
+++ test/test-4-threads.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -m # Enable Job Control
+
+# utils
+error() {
+  echo -n "Error! $1 || at "; caller; exit 1;
+}
+rstr() {
+  tr -dc A-Za-z0-9 </dev/urandom | head -c $1 ; echo ''
+}
+
+
+ROOT="/tmp/mnt"
+TEST="test-4"
+echo "starting" $TEST
+
+cd $ROOT || error
+
+rm -rf $TEST || error
+mkdir $TEST || error
+cd $TEST || error
+
+child_process() {
+  n=${1:-10}
+  c=${2:-"a"}
+#  echo "($n, $c)"
+  for i in $(seq 1 $n); do
+#    echo $i
+    echo -n $c >> x.txt
+    sleep 0.1
+  done
+}
+
+num=50
+
+for i in $(seq 1 $num); do
+  echo -n "fork process $i: "
+  c=$(printf "\x$(printf %x $[64 + $i])")
+  echo $i, $c
+  child_process $i $c &
+done
+
+sleep $[$num / 10 + 2]
+
+#cat x.txt || error
+res=$(cat x.txt) || error
+echo ${#res}
+echo $[($num + 1) * $num / 2]
+if [ ${#res} != $[($num + 1) * $num / 2] ]; then error; fi
+
+
+echo "Success!"
